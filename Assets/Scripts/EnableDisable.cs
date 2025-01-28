0dc0adbefc4c03981327edb450648699f3d073b4
Joey Lam <lamjoey@shernet.sheridancollege.ca>
2025-01-28 16:50:47 -0500

    Finished Turning on and off exercise.

---
+++ Assets/Scripts/EnableDisable.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class EnableDisable : MonoBehaviour
+{
+    public GameObject go;
+    public SpriteRenderer sr;
+    public EnableDisable script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            sr.enabled = false;
+            go.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sr.enabled = true;
+            go.SetActive(true);
+        }
+
+    }
+}
