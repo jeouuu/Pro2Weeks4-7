0618f4a6b87374080dc5231f8069f6caf03d62d1
Joey Lam <lamjoey@shernet.sheridancollege.ca>
2025-02-04 16:12:16 -0500

    Week 5 Exercise - Timer.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour
+{
+
+    Slider slider;
+    float t;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    
+    void Update()
+    {
+        t += Time.deltaTime;
+        slider.value = t ;
+    }
+}
